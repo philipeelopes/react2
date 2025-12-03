c2fda880f13a2d8c402401f68679776de0e45230
philipe <philipedarosa10@gmail.com>
2025-12-03 13:24:48 -0300

    jsx

--- react2/react2/src/App.tsx
+++ react2/react2/src/App.tsx
@@ -1,16 +1,31 @@
-
+import { useState, use } from 'react'
 
 
 import './App.module.css'
 
 
+
+
+const Test = () => {
+  return(
+    <div></div>
+  )
+}
+
 export function App() {
   
   return (
+    <div>
 
-    <button>ola</button>
+        <button>ola</button>
+        <Test></Test>
         
+    </div>
+  
   
   )
 }
+
+
+
 
